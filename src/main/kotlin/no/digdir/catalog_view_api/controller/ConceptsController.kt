18930fcfae2f6ce57e0c15029eee0392b2f7df00
NilsOveTen <github.nils.ove@tendenes.com>
2023-12-15 14:45:16 +0100

    feat: param changedAfter

--- src/main/kotlin/no/digdir/catalog_view_api/controller/ConceptsController.kt
+++ src/main/kotlin/no/digdir/catalog_view_api/controller/ConceptsController.kt
@@ -3,6 +3,7 @@ package no.digdir.catalog_view_api.controller
 import no.digdir.catalog_view_api.model.Concept
 import no.digdir.catalog_view_api.service.ConceptsViewService
 import no.digdir.catalog_view_api.service.PermissionService
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -10,7 +11,9 @@ import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
 
 @RestController
 @RequestMapping(value = ["/catalogs/{catalog}/concepts"], produces = ["application/json"])
@@ -22,10 +25,11 @@ class ConceptsController(
     @GetMapping
     fun getConcepts(
         @AuthenticationPrincipal jwt: Jwt,
-        @PathVariable catalog: String
+        @PathVariable catalog: String,
+        @RequestParam(value = "changedAfter", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) changedAfter: Instant?
     ): ResponseEntity<List<Concept>> =
         if (permissionService.hasOrgReadPermission(jwt, catalog)) {
-            ResponseEntity(conceptsViewService.getConcepts(catalog), HttpStatus.OK)
+            ResponseEntity(conceptsViewService.getConcepts(catalog, changedAfter), HttpStatus.OK)
         } else {
             ResponseEntity(HttpStatus.FORBIDDEN)
         }
