54cd384b84cc3eb68f321e9ed5b57370886e6f88
Nils Ove Tendenes <nils.ove@tendenes.com>
2026-08-14 08:52:40 +0200

    chore: add linting

---
+++ src/main/kotlin/no/digdir/catalogview/config/PostgresConf.kt
@@ -0,0 +1,32 @@
+package no.digdir.catalogview.config
+
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.jdbc.core.JdbcTemplate
+import javax.sql.DataSource
+
+@Configuration
+open class PostgresConf(private val postgresProperties: PostgresProperties) {
+    @Bean
+    open fun conceptCatalogDataSource(): DataSource = DataSourceBuilder
+        .create()
+        .url(postgresProperties.conceptCatalog.url)
+        .username(postgresProperties.conceptCatalog.username)
+        .password(postgresProperties.conceptCatalog.password)
+        .build()
+
+    @Bean
+    open fun adminServiceDataSource(): DataSource = DataSourceBuilder
+        .create()
+        .url(postgresProperties.adminService.url)
+        .username(postgresProperties.adminService.username)
+        .password(postgresProperties.adminService.password)
+        .build()
+
+    @Bean
+    open fun conceptCatalogJdbc(conceptCatalogDataSource: DataSource): JdbcTemplate = JdbcTemplate(conceptCatalogDataSource)
+
+    @Bean
+    open fun adminServiceJdbc(adminServiceDataSource: DataSource): JdbcTemplate = JdbcTemplate(adminServiceDataSource)
+}
---
+++ src/main/kotlin/no/digdir/catalogview/model/AdminModels.kt
@@ -0,0 +1,32 @@
+package no.digdir.catalogview.model
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class AdminCode(val id: String, val name: LocalizedStrings, val parentID: String?)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class CodeList(val id: String, val catalogId: String, val codes: List<AdminCode>)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class AdminUser(val id: String, val catalogId: String, val name: String, val email: String?, val telephoneNumber: String?)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Field(
+    val id: String,
+    val label: LocalizedStrings,
+    val description: LocalizedStrings,
+    val catalogId: String,
+    val type: String,
+    val codeListId: String?,
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class EditableFields(val catalogId: String, val domainCodeListId: String?)
+
+data class CatalogAdminData(
+    val codeLists: Map<String, CodeList>,
+    val domainCodeList: Map<String, String?>,
+    val internalFields: Map<String, Field>,
+    val users: Map<String, AdminUser>,
+)
